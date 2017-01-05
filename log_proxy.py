038c995294dd656c2ca9f96f2194dcafd075d825
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2017-01-05 17:33:30 +0100

    ForwardingLogger: ability to select a log file at run time
    The ForwardingLogger fowards messages above a certain level (by default: all
    messages) to a configured parent logger. Optionally it can prepend the
    configured "forward_prefix" to all *forwarded* log messages.
    
    Python's default logging module can not handle this because
      a) a logger's log level is only applied for messages emitted directly on
         that logger (not for propagated log messages), see
         http://mg.pov.lt/blog/logging-levels.html
      b) adding a log prefix only for certain loggers can only by done by
         duplicating handler configuration. Python's handlers are quite basic
         so if the duplicated handlers access a shared resource (e.g. a log file)
         Python will open it twice (which causes data loss if mode='w' is
         used).
      c) and last but not least we often need to configure the specific logging
         handlers dynamically (e.g. log to a context-dependent file) which is
         not doable via Python's fileConfig either - so we can go fully dynamic
         here...

--- log_proxy.py
+++ log_proxy.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2013-2016 Felix Schwarz
+# Copyright (c) 2013-2017 Felix Schwarz
 # The source code contained in this file is licensed under the MIT license.
 """
 logging is often helpful to find problems in deployed code.
@@ -26,14 +26,77 @@ This library should solve all these problems with a helper function:
 
 import logging
 
-__all__ = ['get_logger', 'l_', 'log_']
+__all__ = ['contextfile_logger', 'get_logger', 'l_', 'log_', 'ForwardingLogger']
 
+# This is added for backwards-compatibility with Python 2.6
 class NullLogger(logging.Logger):
     def _log(self, *args, **kwargs):
         pass
 
     def handle(self, record):
         pass
+
+
+class ForwardingLogger(logging.Logger):
+    """
+    This logger fowards messages above a certain level (by default: all messages)
+    to a configured parent logger. Optionally it can prepend the configured
+    "forward_prefix" to all *forwarded* log messages.
+
+    Python's default logging module can not handle this because
+      a) a logger's log level is only applied for messages emitted directly on
+         that logger (not for propagated log messages), see
+         http://mg.pov.lt/blog/logging-levels.html
+      b) adding a log prefix only for certain loggers can only by done by
+         duplicating handler configuration. Python's handlers are quite basic
+         so if the duplicated handlers access a shared resource (e.g. a log file)
+         Python will open it twice (which causes data loss if mode='w' is
+         used).
+      c) and last but not least we often need to configure the specific logging
+         handlers dynamically (e.g. log to a context-dependent file) which is
+         not doable via Python's fileConfig either - so we can go fully dynamic
+         here...
+    """
+    def __init__(self, *args, **kwargs):
+        self._forward_to = kwargs.pop('forward_to')
+        self._forward_prefix = kwargs.pop('forward_prefix', None)
+        self._forward_minlevel = kwargs.pop('forward_minlevel', logging.NOTSET)
+        super(ForwardingLogger, self).__init__(*args, **kwargs)
+
+    def callHandlers(self, record):
+        super(ForwardingLogger, self).callHandlers(record)
+        # "logging.NOTSET" (default) is defined as 0 so that works here just fine
+        if record.levelno >= self._forward_minlevel:
+            msg = record.msg
+            if self._forward_prefix:
+                msg = self._forward_prefix + msg
+            self._forward_to.log(record.levelno, msg, *record.args)
+
+def contextfile_logger(logger_name, log_path, **kwargs):
+    """
+    Return a ForwardingLogger which logs to the given logfile.
+
+    This is a generic example how to use the ForwardingLogger and can be used
+    to create log files which are placed near the data they are referring to.
+    """
+    log = ForwardingLogger(logger_name,
+        forward_to=kwargs.pop('forward_to'),
+        forward_prefix=kwargs.pop('forward_prefix', None),
+        forward_minlevel=kwargs.pop('forward_minlevel', logging.NOTSET),
+        **kwargs
+    )
+    # The logging module does not keep a reference to this FileHandler anywhere
+    # as we are instantiating it directly (not by name or fileconfig).
+    # That means Python's garbage collection will work just fine and the
+    # underlying log file will be closed when our batch-specific
+    # ForwardingLogger goes out of scope.
+    file_handler = logging.FileHandler(log_path)
+    file_handler.setFormatter(logging.Formatter(
+        fmt='%(asctime)s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    ))
+    log.addHandler(file_handler)
+    return log
 
 
 class ContextAdapter(logging.LoggerAdapter):
