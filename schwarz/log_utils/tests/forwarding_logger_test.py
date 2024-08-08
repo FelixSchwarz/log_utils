a58ff7b0a20f7510c84c8d076f9a27ed99de5a34
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2024-08-08 17:47:48 +0200

    remove dependency on PythonicTestcase

--- schwarz/log_utils/tests/forwarding_logger_test.py
+++ schwarz/log_utils/tests/forwarding_logger_test.py
@@ -8,46 +8,51 @@ from __future__ import absolute_import, print_function, unicode_literals
 import logging
 from logging import Formatter as LogFormatter
 
-from pythonic_testcase import PythonicTestCase
+import pytest
 from testfixtures import LogCapture
 
 from .. import get_logger, ForwardingLogger
 from ..testutils import LogHelper
 
 
-class ForwardingLoggerTest(PythonicTestCase):
-    def setUp(self):
-        self.log_helper = LogHelper.set_up(test=self)
+@pytest.fixture
+def log_helper():
+    _log_helper = LogHelper.set_up()
+    try:
+        yield
+    finally:
+        _log_helper.cleanup()
 
-    def test_can_forward_with_prefix(self):
-        with LogCapture() as l_:
-            log = get_logger('bar')
-            wrapped_log = ForwardingLogger(forward_to=log, forward_prefix='[ABC] ', forward_minlevel=logging.INFO)
-            wrapped_log.info('hello world')
-            wrapped_log.debug('should be ignored')
-        l_.check(('bar', 'INFO', '[ABC] hello world'),)
 
-    def test_can_forward_with_suffix(self):
-        with LogCapture() as l_:
-            log = get_logger('bar')
-            wrapped_log = ForwardingLogger(forward_to=log, forward_suffix=' (foo)')
-            wrapped_log.info('hello world')
-        l_.check(('bar', 'INFO', 'hello world (foo)'),)
+def test_forwardinglogger_can_forward_with_prefix(log_helper):
+    with LogCapture() as l_:
+        log = get_logger('bar')
+        wrapped_log = ForwardingLogger(forward_to=log, forward_prefix='[ABC] ', forward_minlevel=logging.INFO)
+        wrapped_log.info('hello world')
+        wrapped_log.debug('should be ignored')
+    l_.check(('bar', 'INFO', '[ABC] hello world'),)
 
-    def test_can_forward_exception_info(self):
-        exc_msg = 'something went wrong'
-        with LogCapture() as l_:
-            log = get_logger('bar')
-            wrapped_log = ForwardingLogger(forward_to=log, forward_prefix='[FOO] ')
-            try:
-                raise ValueError(exc_msg)
-            except ValueError:
-                wrapped_log.exception('caught error')
+def test_forwardinglogger_can_forward_with_suffix(log_helper):
+    with LogCapture() as l_:
+        log = get_logger('bar')
+        wrapped_log = ForwardingLogger(forward_to=log, forward_suffix=' (foo)')
+        wrapped_log.info('hello world')
+    l_.check(('bar', 'INFO', 'hello world (foo)'),)
 
-        l_.check(('bar', 'ERROR', '[FOO] caught error'),)
-        lr, = l_.records
-        logged_text = LogFormatter('%(message)s').format(lr)
-        assert logged_text.startswith('[FOO] caught error')
-        assert 'Traceback (most recent call last)' in logged_text
-        assert ('ValueError: ' + exc_msg) in logged_text
+def test_forwardinglogger_can_forward_exception_info(log_helper):
+    exc_msg = 'something went wrong'
+    with LogCapture() as l_:
+        log = get_logger('bar')
+        wrapped_log = ForwardingLogger(forward_to=log, forward_prefix='[FOO] ')
+        try:
+            raise ValueError(exc_msg)
+        except ValueError:
+            wrapped_log.exception('caught error')
+
+    l_.check(('bar', 'ERROR', '[FOO] caught error'),)
+    lr, = l_.records
+    logged_text = LogFormatter('%(message)s').format(lr)
+    assert logged_text.startswith('[FOO] caught error')
+    assert 'Traceback (most recent call last)' in logged_text
+    assert ('ValueError: ' + exc_msg) in logged_text
 
--- schwarz/log_utils/tests/get_logger_test.py
+++ schwarz/log_utils/tests/get_logger_test.py
@@ -5,56 +5,60 @@
 
 import logging
 
-from pythonic_testcase import PythonicTestCase
+import pytest
 from testfixtures import LogCapture
 
 from ..log_proxy import get_logger
 from ..testutils import LogHelper
 
 
-class GetLoggerTest(PythonicTestCase):
-    def setUp(self):
-        self.log_helper = LogHelper.set_up(test=self, globals_=globals())
+@pytest.fixture
+def log_helper():
+    _log_helper = LogHelper.set_up(globals_=globals())
+    try:
+        yield
+    finally:
+        _log_helper.cleanup()
 
-    def test_can_return_regular_python_loggers(self):
-        with LogCapture() as l_:
-            log = get_logger('bar')
-            log.info('hello world')
-        l_.check(('bar', 'INFO', 'hello world'),)
+def test_get_logger_can_return_regular_python_loggers(log_helper):
+    with LogCapture() as l_:
+        log = get_logger('bar')
+        log.info('hello world')
+    l_.check(('bar', 'INFO', 'hello world'),)
 
-    def test_can_log_to_passed_logger(self):
-        with LogCapture() as l_:
-            bar_logger = logging.getLogger('bar')
-            log = get_logger('foo', log=bar_logger)
-            log.info('logged via bar not foo')
-        l_.check(('bar', 'INFO', 'logged via bar not foo'),)
+def test_get_logger_can_log_to_passed_logger(log_helper):
+    with LogCapture() as l_:
+        bar_logger = logging.getLogger('bar')
+        log = get_logger('foo', log=bar_logger)
+        log.info('logged via bar not foo')
+    l_.check(('bar', 'INFO', 'logged via bar not foo'),)
 
-    def test_can_disable_logging(self):
-        with LogCapture() as l_:
-            log = get_logger('foo', log=False)
-            log.debug('foo %s', 'bar')
-            log.warning('foo %s', 'bar')
-            log.error('foo %s', 'bar')
-            # need to cause an exception so log.exception works...
-            try:
-                log.invalid
-            except:
-                log.exception('foo %s', 'bar')
+def test_get_logger_can_disable_logging(log_helper):
+    with LogCapture() as l_:
+        log = get_logger('foo', log=False)
+        log.debug('foo %s', 'bar')
+        log.warning('foo %s', 'bar')
+        log.error('foo %s', 'bar')
+        # need to cause an exception so log.exception works...
+        try:
+            log.invalid
+        except:
+            log.exception('foo %s', 'bar')
 
-            assert len(l_.records) == 0, \
-                'must not log messages via Python loggers when using "log=False"'
+        assert len(l_.records) == 0, \
+            'must not log messages via Python loggers when using "log=False"'
 
-            # ensure that the fake logger from the beginning of this test does
-            # not make any permanent changes and we can still use regular
-            # loggers.
-            pylog = logging.getLogger('foo')
-            pylog.info('should log this')
-            l_.check(('foo', 'INFO', 'should log this'),)
+        # ensure that the fake logger from the beginning of this test does
+        # not make any permanent changes and we can still use regular
+        # loggers.
+        pylog = logging.getLogger('foo')
+        pylog.info('should log this')
+        l_.check(('foo', 'INFO', 'should log this'),)
 
-    def test_can_pass_log_level(self):
-        with LogCapture() as l_:
-            log = get_logger('bar', level=logging.WARN)
-            log.info('hello world')
-            log.warning('something went wrong!')
-        l_.check(('bar', 'WARNING', 'something went wrong!'),)
+def test_get_logger_can_pass_log_level(log_helper):
+    with LogCapture() as l_:
+        log = get_logger('bar', level=logging.WARN)
+        log.info('hello world')
+        log.warning('something went wrong!')
+    l_.check(('bar', 'WARNING', 'something went wrong!'),)
 
